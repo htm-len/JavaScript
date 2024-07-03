28bf087d48380e3936079b4593e2e2d69f2a1b4f
Lennart Lewis <lenlew22@gmail.com>
2024-07-02 21:23:09 -0400

    Data Types

---
+++ 01-variables-data-types/data-types/script3.js
@@ -0,0 +1,43 @@
+// String 
+const firstName = 'Sara';
+
+// Number
+const age = 30;
+const temperature = 30.5;
+
+// Boolean
+const hasKids = true;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+
+//let score; 
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+//bigInt
+const bigNumber = 9007199254740991n;
+// reference types
+const numbers = [1, 2, 3, 4, 5];
+
+const person = {
+    name: 'John',
+    age: 30,
+};
+
+//function
+function sayHello() {
+    return 'Hello';
+}
+
+
+const output = sayHello;
+
+console.log(output, typeof output);
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/reference/operators/typeof#typeof_null
